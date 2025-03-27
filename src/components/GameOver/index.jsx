c397f935d027c0a16e57722891a26a1d58c663c3
JHenrique <henryque.pc.game@gmail.com>
2025-03-26 21:53:49 -0300

    teste

--- src/components/GameOver/index.jsx
+++ src/components/GameOver/index.jsx
@@ -1,13 +1,14 @@
 import stylles from "./gameOver.module.css";
 
-
 // eslint-disable-next-line react/prop-types
-export default function GameOver({retry, score}) {
-    return (
-        <div className={stylles}>
-            <h2>Game Over</h2>
-            <h2>A sua pontuação foi: <span>{score}</span></h2>
-            <button onClick={retry}>Resetar Jogor</button>
-        </div>
-    )
+export default function GameOver({ retry, score }) {
+  return (
+    <div className={stylles}>
+      <h2>Game Over</h2>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retry}>Resetar Jogor</button>
+    </div>
+  );
 }
